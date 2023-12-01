f3ec2cf0db38dae5f9ed460f8d0e1182b8d7c291
Olitoni <olitoniyt@gmail.com>
2023-12-01 21:19:20 +0100

    update

--- app/src/main/java/com/example/twojastara/MainActivity.kt
+++ app/src/main/java/com/example/twojastara/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.twojastara
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.twojastara.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {  }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main, fragment)
     }
 }
