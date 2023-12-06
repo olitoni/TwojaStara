95b6509ea87448f0b6b0e509a99614f8c48c263a
Olitoni <olitoniyt@gmail.com>
2023-12-06 08:09:57 +0100

    delete frgament

--- app/src/main/java/com/example/twojastara/MainActivity.kt
+++ app/src/main/java/com/example/twojastara/MainActivity.kt
@@ -12,31 +12,31 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.home -> replaceFragment(Home())
-                else -> {
-
-                }
-            }
-            true
-        }
-
-        val bottomNav = findViewById<View>(R.id.bottomNavigationView) as BottomNavigationView
-        bottomNav.menu.findItem(R.id.home).setChecked(true)
-
-        replaceFragment(Home())
+//
+//
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        binding.bottomNavigationView.setOnItemSelectedListener {
+//            when(it.itemId) {
+//                R.id.home -> replaceFragment(Home())
+//                else -> {
+//
+//                }
+//            }
+//            true
+//        }
+//
+//        val bottomNav = findViewById<View>(R.id.bottomNavigationView) as BottomNavigationView
+//        bottomNav.menu.findItem(R.id.home).setChecked(true)
+//
+//        replaceFragment(Home())
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame, fragment)
-        fragmentTransaction.commit()
-    }
+//    private fun replaceFragment(fragment: Fragment) {
+//        val fragmentManager = supportFragmentManager
+//        val fragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.frame, fragment)
+//        fragmentTransaction.commit()
+//    }
 }
