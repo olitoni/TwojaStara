f4afc8aca16edc49d8adc0a9ab0470418ef91f36
Olitoni <olitoniyt@gmail.com>
2023-12-06 08:10:53 +0100

    clean

--- app/src/main/java/com/example/twojastara/MainActivity.kt
+++ app/src/main/java/com/example/twojastara/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.twojastara
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.example.twojastara.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
