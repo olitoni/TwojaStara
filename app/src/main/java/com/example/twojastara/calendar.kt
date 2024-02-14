c1b714b8d8b75e0841bda22264312213b264b6ef
Olitoni <olitoniyt@gmail.com>
2024-02-15 00:41:24 +0100

    :( list

--- app/src/main/java/com/example/twojastara/calendar.kt
+++ app/src/main/java/com/example/twojastara/calendar.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import java.util.Calendar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +38,9 @@ class calendar : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false)
+        val lv = R.id.listView as ListView
+        val adapter = ArrayAdapter(this.requireContext(), android.R.layout.activity_list_item,listOf("car", "plane"))
+        lv.adapter = adapter
     }
 
     companion object {
