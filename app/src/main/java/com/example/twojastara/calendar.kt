8a36e68d0870cd75239e322f9b22024ffde09734
Olitoni <olitoniyt@gmail.com>
2024-02-29 10:30:50 +0100

    db date

--- app/src/main/java/com/example/twojastara/calendar.kt
+++ app/src/main/java/com/example/twojastara/calendar.kt
@@ -11,11 +11,16 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomappbar.BottomAppBar.FabAlignmentMode
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 
 
 lateinit var recyclerView: RecyclerView
 lateinit var mcv: MaterialCalendarView
+lateinit var fab: FloatingActionButton
 
 class calendar : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,14 +33,15 @@ class calendar : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_calendar, container, false)
-        mcv = root.findViewById(R.id.calendarView)
-        mcv.selectionColor = Color.parseColor("#51FFFFFF")
-        mcv.addDecorator(CurrentDayDecorator(activity))
-        recyclerView = root.findViewById(R.id.recyclerView)
         val db = DBHelper(this.requireContext(), null)
+        mcv = root.findViewById(R.id.calendarView)
+        recyclerView = root.findViewById(R.id.recyclerView)
+        fab = root.findViewById(R.id.fab)
 
+        mcv.selectionColor = Color.parseColor("#20FFFFFF")
+        mcv.addDecorator(CurrentDayDecorator(activity))
+        mcv.selectedDate = CalendarDay.today()
         mcv.setOnDateChangedListener { _, date, _ ->
-            db.sql("INSERT INTO Events VALUES(1, 'Piotr', '18a')")
             Toast.makeText(context, date.toString(), Toast.LENGTH_SHORT).show()}
 
         val eventList = mutableListOf<EventItem>()
@@ -54,24 +60,38 @@ class calendar : Fragment() {
         }
         cursor.close()
 
-//        for (i in 1..15){
-//            val name = "Item $i"
-//            val desc = "desc $i"
-//
-//            val foodItem = EventItem(name = name, desc = desc)
-//
-//
-//            eventList.add(foodItem)
-//        }
-
-//        val dates = ArrayList<CalendarDay>()
-//        val mydate= CalendarDay.from(2024,  2, 4)
-//        dates.add(mydate)
-
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this.requireContext())
 
+        fab.setOnClickListener { view ->
+            db.sql("INSERT INTO Events VALUES(null, 'Event', 'data')")
+            Snackbar.make(view, "Dodano", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+            eventList.clear()
+            val db = DBHelper(this.requireContext(), null)
+            val cursor = db.query("SELECT * FROM Events")
+            if (cursor!!.moveToFirst()) {
+                do {
+                    val name = cursor.getString(cursor.getColumnIndex("name"))
+                    val age = cursor.getString(cursor.getColumnIndex("age"))
+
+                    val foodItem = EventItem(name = name, desc = age)
+                    eventList.add(foodItem)
+                }
+                while(cursor.moveToNext())
+            }
+            cursor.close()
+
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager = LinearLayoutManager(this.requireContext())
+        }
+
         return root
+    }
+
+    fun fetchEvents() {
+
     }
 
     companion object {
